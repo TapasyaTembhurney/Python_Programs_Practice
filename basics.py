347d6bbdb4ddd7bd7ba8c20bc4fc6308a862ed94
Your name <tapasyatembhurney@gmail.com>
2025-10-28 00:23:05 +0530

    Basics of Python

---
+++ basics.py
@@ -0,0 +1,31 @@
+# a = int(input("Enter a number : "))
+# print(a)
+
+
+# a = 3
+# b = 6
+
+# print("The sum is :" ,a+b)
+
+
+# COMMENTS
+"""kdsjoijk
+kldkdwjij4n
+mdoiwjij"""
+
+# ESCAPE SEQUENCES
+
+print("Hello Python Programming . I am in my learning phase.")
+print("Hello Python Programming \nI am in my learning phase.")
+print("Hello Python Programming .\tI am in my learning phase.")
+print("Hello Python Programming .\\ I am in my learning phase.")
+print("Hello Python Programming. \" I am in my learning phase.")
+print("Hello Python Programming. \' I am in my learning phase.")
+
+# Print statement
+
+
+print("Hello World!" , "Harry" , 5 )
+print("Hello World!" , "Harry" , 5 , sep= " ,")
+print("Hello World!" , "Harry" , 5 , sep= " / ")
+
