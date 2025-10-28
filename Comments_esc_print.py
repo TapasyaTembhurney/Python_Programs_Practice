52101290eb699421fcf49f71e9d5306d7643cc65
Your name <tapasyatembhurney@gmail.com>
2025-10-28 23:01:48 +0530

    basics2

---
+++ Comments_esc_print.py
@@ -0,0 +1,35 @@
+a = 3
+b = 6
+
+print("The sum is :" ,a+b)
+
+
+# COMMENTS
+
+# using single hash(#)
+# using three single quotes or three double quotes
+
+'''kdsjoijk
+kldkdwjij4n
+mdoiwjij'''
+
+"""kdsjoijk
+kldkdwjij4n
+mdoiwjij"""
+
+# ESCAPE SEQUENCES
+
+print("Hello Python Programming . I am in my learning phase.")
+print("Hello Python Programming \nI am in my learning phase.")
+print("Hello Python Programming .\tI am in my learning phase.")
+print("Hello Python Programming .\\ I am in my learning phase.")
+print("Hello Python Programming. \" I am in my learning phase.")
+print("Hello Python Programming. \' I am in my learning phase.")
+
+# Print statement
+
+
+print("Hello World!" , "Harry" , 5 )
+print("Hello World!" , "Harry" , 5 , sep= " ,")
+print("Hello World!" , "Harry" , 5 , sep= " / ")
+
