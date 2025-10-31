5c1bc90be3f09fa3f6d83e2cfd826e9186a67f5a
Your name <tapasyatembhurney@gmail.com>
2025-10-31 07:34:54 +0530

    for_loop

---
+++ for_Loop.py
@@ -0,0 +1,16 @@
+for i in range (1,8): # range function goes from 1 to 7 in this case .
+    print(i)
+
+print("\n-----------------\n")
+
+
+#Table for any number
+num = int(input("Enter a number : "))
+
+for j in range(1,11):
+    print(num, "X ", j , "=", num*(j))
+
+
+#Just for checking the code
+# x = int("twenty-five")
+# print(x)
