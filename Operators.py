7c59cdf15d1484baa6c570327571ab67ae02e5ca
Your name <tapasyatembhurney@gmail.com>
2025-10-31 06:49:02 +0530

    Operators

---
+++ Operators.py
@@ -0,0 +1,81 @@
+# REPL -> Read Evaluate Print Loop 
+# ctrl + cursor keys
+# alt + cursor keys 
+
+a = 99
+b = 10
+
+#ARITHMETIC OPERATORS
+print("\n")
+print("ARITHMETIC OPERATORS")
+
+print("a + b = " , a + b)
+print("a - b = " , a - b)
+print("a * b = " , a * b)
+print("a / b = " , a / b)
+print("a % b = " , a % b)
+print("a // b = " , a // b)
+print("a ** b = " , a ** b)
+print("a ^ b = " , a ^ b)
+print("\n")
+
+#COMPARISON OPERATORS
+
+print("COMPARISON OPERATORS")
+
+print(a > 4)
+print(a < 4)
+print(a <= 4)
+print(a >=4)
+print(a == 4) #Is a equal to 4?
+print(a == 99)#Is a equal to 99?
+print(a != 99)#Is a not equal to 34?
+print("\n")
+#lOGICAL OPERATORS
+print("LOGICAL OPERATORS")
+c = True
+d = False
+
+print(c and d)
+print(c or d)
+print(not c)
+print(not d)
+print("\n")
+print(not(True))
+print(not(False))
+
+#ASSIGNMENT OPERATORS
+print("\n")
+print("ASSIGNMENT OPERATORS")
+
+e = 5
+f = 7
+
+e += 5
+print(e)
+
+f -= 7
+print(f)
+
+g = 10
+h = 2
+
+g /= 10
+print(g)
+h *= 2
+print(h)
+
+
+i = 30
+j = 40
+
+i %= 10
+print(i)
+j **= 2
+print(j)
+
+k = 20
+k //= 10
+print(k)
+
+
