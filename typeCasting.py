5a182a07b598a38ebc782f3b69958adc4397bc42
Your name <tapasyatembhurney@gmail.com>
2025-10-31 07:25:34 +0530

    typeCasting

---
+++ typeCasting.py
@@ -0,0 +1,27 @@
+# Variables
+a = 10
+b = "223"
+
+#built-in functions for typecasting
+
+print(a)
+print(type(a))
+
+print(b)
+print(type(b))
+
+# Conversion of datatype
+c = int(b)
+print(c)
+
+d = float(b)
+print(d)
+print(type(d))
+
+e = bool(d)
+print(e)
+print(type(e))
+
+f = str(a)
+print(a)
+print(type(f))
