4a1dfa98cc7350174f411c72c054c9d251f5444b
Your name <tapasyatembhurney@gmail.com>
2025-11-02 14:15:09 +0530

    String_StringSlicing_Indexing

---
+++ String_StringSlicing_Indexing.py
@@ -0,0 +1,31 @@
+# name = "Ishika1"
+# name = 'Harry2'
+# name = '''Ishan'''
+name = '''Ishan is a good boy'''
+print(name)
+
+name1 = "Sam"
+print(name1[0])
+
+print(name1[0] , name1[1] , name1[2])
+print("\n")
+print(name1[-1])
+print(name1[-2])
+print(name1[-3])
+
+name2 = "Harry"
+print(name2[0:2])
+print(name2[2:-1])
+print("\n")
+text = "Hello, Python!"
+print(text[0:5])   # Output: Hello
+print(text[:5])    # Output: Hello (same as text[0:5])
+print(text[7:])    # Output: Python! (from index 7 to end)
+print(text[::2])   # Output: Hlo Pto!
+print(text[-6:-1]) # Output: ython (negative indexing)
+
+print(text[0:12:1])
+print(text[0:7:2])
+
+print(name[1:])
+print(name[:4])
