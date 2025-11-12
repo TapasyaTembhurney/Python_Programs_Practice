0034ac8f981a193048f694fa5514e4ce558a4548
Your name <tapasyatembhurney@gmail.com>
2025-11-12 07:33:15 +0530

    defining_function_in_Python

---
+++ defining_functions_in_Python.py
@@ -0,0 +1,32 @@
+# a = 1
+# b = 2
+# c = 3
+# average = (a+b+c)//3 #(floor division)
+# print(average)
+
+# a1 = 7
+# b1 = 3
+# c1 = 7
+# average1 = (a+b+c)/3.0#(normal division)
+# print(average1)
+
+
+
+#Now if want to make changes everywhere by just making changes only at one point then you can do by function.
+
+#1
+def average(a,b,c):
+    avg = (a+b+c)//3
+    # print(avg)
+    return avg
+
+o1 = average(3,4,5)
+print(o1)
+
+
+
+#2
+def greet(name):
+    return (f"Hello, {name}!")
+print(greet("Alice"))
+
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,5 @@
-if(char in vowels):
-    #     sum += 1
+a1 = 7
+# b1 = 3
+# c1 = 7
+# average1 = (a+b+c)/3.0#(normal division)
+# print(average1)
