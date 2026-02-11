f12391dd7ef5a12cee96198eb1c91a9be5bcb74c
Your name <tapasyatembhurney@gmail.com>
2026-02-11 13:26:33 +0530

    Lists

---
+++ lists.py
@@ -0,0 +1,18 @@
+marks = [100 , 89 , 99 , 79 , 78]
+
+mixed_type_list = [20 , 7.8 , True , "DSA", 'DSA']
+
+"""
+Lists can store multiple values of any datatype
+Lists are :- Ordered , mutable(changeable) collection if items.
+"""
+
+print(marks)
+print(marks, marks[4]) #Indexing
+
+# print(marks, marks[4] , end= " ")
+
+print(mixed_type_list)
+
+print(mixed_type_list[2:4]) #Slicing
+
