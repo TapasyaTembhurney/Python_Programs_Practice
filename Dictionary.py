bbd788dc6156140827f6c9311d5ed33cc2c427af
Your name <tapasyatembhurney@gmail.com>
2026-03-20 12:27:23 +0530

    Dictionary_&_Methods

---
+++ Dictionary.py
@@ -0,0 +1,41 @@
+marks = {"Harry" : 34, "Jack" : 44 , "Sia" : 32}
+
+print(marks , type(marks))
+
+print("\n")
+print(marks["Harry"])
+print(marks["Sia"])
+
+marks["Harry"] = 3 # Updation in dictionary
+print(marks)
+
+# ----------------------------------------------------
+
+# DICTIONARY METHODS
+
+print(marks.keys()) #Gives all the keys in o/p
+print(marks.values()) #Gives all the values of keys in o/p
+
+marks.pop("Sia" , "Harry") 
+# marks.clear()
+print(marks)
+
+v = marks.pop("Sia" , "Harry") 
+# marks.clear()
+print(v)
+
+print("The copy of the marks dictionary is :",marks.copy())
+
+print(marks.fromkeys("Hampi"))
+
+print(marks.get("Harry"))
+
+print(marks.items())
+
+# print(marks.popitem())
+
+upd_dictionary = marks.setdefault('Taylor', 99)
+print(upd_dictionary)
+
+print(marks)
+
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,3 +1,1 @@
-new_set.remove(2)
-# new_set.discard(4)
-# new_set.pop()
+"Sia"
