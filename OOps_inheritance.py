1ee8974319293d745de94e3f100e4838ecd66092
Your name <tapasyatembhurney@gmail.com>
2026-03-29 20:59:03 +0530

    OOPS_Inheritance & Super() method is used

---
+++ OOps_inheritance.py
@@ -0,0 +1,21 @@
+class Animal: #Parent class
+    
+    location = "Australia"
+
+    def __init__(self, name):  
+        self.name = name
+
+    def speak(self):
+        print("Speaking Now ...")
+
+class Dog(Animal): #This is how inheritance is done in Python
+    def speak(self):
+        super().speak() # We are using speack function of the parent class.
+        print("Woof!")
+
+a = Animal("Jessy")
+a.speak()
+
+d = Dog("Bruno")
+d.speak()
+print(d.location)
