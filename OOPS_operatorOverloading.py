dff8170c3154faed6f299f89230e3b64524f6947
Your name <tapasyatembhurney@gmail.com>
2026-03-30 17:49:02 +0530

    Added __add__ function which overloaded the plus(+) operator

--- OOPS_operatorOverloading.py
+++ OOPS_operatorOverloading.py
@@ -8,9 +8,13 @@ class Point:
     
     def print_point(self):
         print(f"X is {self.x} and Y is {self.y}") 
+
+    def __add__(self, p):
+        return Point((self.x + p.x), (self.y + p.y))
     
 p1 = Point(3,7)
 p2 = Point(2,9)
 
-p = p1.sum(p2)
+# p = p1.sum(p2) # Returns a new point which is sum of p1 and p2
+p = p1 + p2 # We overloaded the + operator by writing the add function
 p.print_point()
