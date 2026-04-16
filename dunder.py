107bd138a15e2e9055b5dd07f328e3d7fc60b7c6
Your name <tapasyatembhurney@gmail.com>
2026-04-16 23:55:33 +0530

    Dunder method

--- dunder.py
+++ dunder.py
@@ -1,14 +1,20 @@
 class Employee:
     company = "HP"
-    def __init__(self, name , salary):
+    
+    def __init__(self, name, salary):
         self.name = name
         self.salary = salary
     
     def display(self):
-        info = f"The name is {self.name} and the salary is {self.salary}"
-    def display(self):
-        info = f"The name is {self.name} and the salary is {self.salary}"
-        
-    e = Employee("Harry", 352)
-    print(e.name , e.salary)
-    print(str(e))
+        return f"The name is {self.name} and the salary is {self.salary}"
+    
+    def __str__(self): #dunder method
+        return f"Employee(Name: {self.name}, Salary: {self.salary})"
+
+
+# Creating object outside the class
+e = Employee("Harry", 352)
+
+print(e.name, e.salary)        # Access attributes
+print(e.display())             # Call method
+print(str(e))                  # Uses __str__()
